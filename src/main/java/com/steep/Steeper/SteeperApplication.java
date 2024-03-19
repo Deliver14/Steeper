fa1c398ea8bc015b135253e75c4950ed0afd06f1
Deliver14 <Hoaglandc94@gmail.com>
2024-03-19 11:35:15 -0500

    initial commit

---
+++ src/main/java/com/steep/Steeper/SteeperApplication.java
@@ -0,0 +1,13 @@
+package com.steep.Steeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/steep/Steeper/SteeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steep.Steeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
